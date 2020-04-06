4a9d10bc628c8d8549dabcdd40d458c29062dab6
Savanah Steigen <savanahsteigen@gmail.com>
2020-04-06 16:20:24 -0400

    Updates

---
+++ precache-manifest.2a1a5f8a344bb3846c10fe26ce8d2717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b446fe35703f86991911e9535a2915d4",
+    "url": "/skatesona/index.html"
+  },
+  {
+    "revision": "1722661d5e482642169b",
+    "url": "/skatesona/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a80e78c3c928def6a9c4",
+    "url": "/skatesona/static/js/2.0441eae2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/skatesona/static/js/2.0441eae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1722661d5e482642169b",
+    "url": "/skatesona/static/js/main.e871b04e.chunk.js"
+  },
+  {
+    "revision": "1fca4488ffe71aac4ff7",
+    "url": "/skatesona/static/js/runtime-main.237b1604.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/skatesona/static/media/logo.5d5d9eef.svg"
+  }
+]);
